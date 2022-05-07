d45a8fd69b225f84b97e780e42e71b96fefd32be
vk <vkotl@protonmail.com>
2022-05-07 19:22:08 +0300

    changed according to google style guide

---
+++ zsh/.scripts/dutils.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Library with common utilities for interraction with dmenu.
+
+#######################################
+# Extract urls from stream.
+# Arguments:
+#   Stream which should contain URLs.
+# Outputs:
+#   Array of URL's. 
+#######################################
+dutils::grep_urls() {
+  local URLREGEX="(((http|https|gopher|gemini|ftp|ftps|git)://|www\\.)"
+  URLREGEX+="[a-zA-Z0-9.]*[:]?[a-zA-Z0-9./@$\+&%?$\#=_~-]*)|"
+  URLREGEX+="((magnet:\\?xt=urn:btih:)[a-zA-Z0-9]*)"
+  echo "$1" | grep -aEo $URLREGEX
+}
+
+dutils::einput() {
+  /dev/null 2>/dev/null | dmenu "$@"
+}
+
+dutils::notify() {
+  notify-send -a "$APP" "$@"
+}
+
+dutils::cnotify() {
+  notify-send -a "$APP" -u critical "$@"
+}
+
+dutils::lnotify() {
+  notify-send -a "$APP" -u low "$@"
+}
