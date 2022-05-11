a23e038ddb9493ef05fb8868499bc9b5bc6c5d11
vk <vkotl@protonmail.com>
2022-05-11 21:45:59 +0300

    remove animations from firefox

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -1,3 +1,8 @@
 user_pref("browser.uidensity", 1);
 user_pref("signon.rememberSignons", false);
 user_pref("layers.acceleration.force-enabled", true);
+user_pref("toolkit.scrollbox.smoothScroll", false);
+user_pref("gfx.webrender.all", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.download.animateNotifications", false);
+user_pref("image.mem.animated.discardable", false);
